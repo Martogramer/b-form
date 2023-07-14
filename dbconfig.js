ce4b0646b0541870db599bb443d58a4f1df4f564
Martin Iribas <martin.iribass@gmail.com>
2023-07-14 00:25:23 -0300

    -

--- dbconfig.js
+++ dbconfig.js
@@ -1,5 +1,5 @@
 
 module.exports = {
-    mongoURL: 'mongodb+srv://telescopiens:RCbMwVsA91kkqgVN@cluster0.6m81jx3.mongodb.net/?retryWrites=true&w=majority',
+    mongoURL: 'mongodb+srv://telescopiens:uQttzxizwgg9Hlu3@cluster0.6m81jx3.mongodb.net/?retryWrites=true&w=majority',
     secretKey: 'clave_secreta_para_jsonwebtoken'
   };
