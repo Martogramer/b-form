370d36647cae41d7fa00692a2f97b5b38e1e4acc
SadiLuis <sadiluisrueda.94@gmail.com>
2023-08-09 10:17:56 -0300

    Change Dotenv

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,5 +1,6 @@
+require ('dotenv').config();
 
 module.exports = {
-  mongoURL: 'mongodb+srv://telescopiens:53jBbF4vqknbj91v@cluster0.6m81jx3.mongodb.net/?retryWrites=true&w=majority',
+  mongoURL: process.env.MONGODB_URI,
   secretKey: 'clave_secreta_para_jsonwebtoken'
 };
